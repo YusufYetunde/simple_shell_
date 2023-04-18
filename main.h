55e7ab869e4ee78fc8f57ce7fb3505b870c5a3e9
Yusuf Yetunde <yusufyetunde13@gmail.com>
2023-04-17 18:28:20 -0700

    main

--- main.h
+++ main.h
@@ -1,4 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
 #endif
