d5c247b46ec02a02877f4e5440afbe2e67a37528
Yusuf Yetunde <yusufyetunde13@gmail.com>
2023-04-24 16:01:16 -0700

    Function for built in exit

--- main.h
+++ main.h
@@ -14,9 +14,21 @@
 #include <ctype.h>
 #include <errno.h>
 
+typedef struct infot
+{
+	int argc;
+	char *arg;
+	char **argv;
+	int status;
+}info_t;
+
 int _interpreter(char *s);
 int is_interactive(void);
 int is_delimiter(char c);
 int has_alphabets(char *s);
+
+int _myexit(info_t *info);
+int _mycd(info_t *info);
+int _myhelp(info_t *info);
 
 #endif
