79d7c18964d014b7dc984d5599532a10056171e5
Yusuf Yetunde <yusufyetunde13@gmail.com>
2023-04-24 18:28:41 -0700

    function for env setting

--- main.h
+++ main.h
@@ -14,6 +14,13 @@
 #include <ctype.h>
 #include <errno.h>
 
+typedef struct lists
+{
+	int *num;
+	char *str;
+	struct lists *next;
+}list_t;
+
 typedef struct infot
 {
 	int argc;
@@ -22,13 +29,11 @@ typedef struct infot
 	int status;
 	char *history;
 	char *alias;
+	list_t *env;
+	char **argv;
+	int argc;
+	char **environ;
 }info_t;
-typedef struct lists
-{
-	int *num;
-	char *str;
-	struct lists *next;
-}list_t;
 
 int _interpreter(char *s);
 int is_interactive(void);
@@ -45,5 +50,11 @@ int set_alias(info_t *info, char *str);
 int print_alias(list_t *node);
 int _myalias(info_t *info);
 
+void add_node_end(list_t **head, char *str);
+void print_list_str(list_t *head);
+char *find_str_in_list(list_t *head, const char *str);
+int my_setenv(info_t *info);
+int my_unsetenv(info_t *info);
+int populate_env_list(info_t *info);
 
 #endif
